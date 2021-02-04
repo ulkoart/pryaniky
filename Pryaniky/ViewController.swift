f34eafe15c983a6ad0dbd5eb484d448db03b5777
Artem Ulko <ulkoart@gmail.com>
2021-02-04 09:04:02 +0300

    Initial Commit

---
+++ Pryaniky/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pryaniky
+//
+//  Created by user on 04/02/2021.
+//  Copyright © 2021 Artem Ulko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
